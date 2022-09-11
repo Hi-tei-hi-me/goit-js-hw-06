87f8a5376fd51575310e9b9c928dc6827c44507c
Hi-tei-hi-me <lethargium@gmail.com>
2022-09-11 22:45:45 +0300

    Finished homework

--- js/task-10.js
+++ js/task-10.js
@@ -1,4 +1,38 @@
 "use strict";
+const boxesQty = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesContainer = document.querySelector("div#boxes");
+boxesContainer.style.marginTop = "10px";
+boxesContainer.style.display = "flex";
+boxesContainer.style.flexDirection = "column";
+boxesContainer.style.rowGap = "8px";
+
+createBtn.addEventListener("click", () => {
+  destroyBoxes();
+  createBoxes(boxesQty.value);
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const galleryItem = document.createElement("div");
+    const sizeParam = 30 + i * 10 + "px";
+    galleryItem.style.height = sizeParam;
+    galleryItem.style.width = sizeParam;
+    galleryItem.style.backgroundColor = getRandomHexColor();
+    galleryItem.style.border = "1px solid #000";
+    boxes.push(galleryItem);
+  }
+  boxesContainer.append(...boxes);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
 }
